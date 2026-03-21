9ae63eb2ab2076701023a795ad71d1dd4055e90e
mzapora <mz.zapora@gmail.com>
2026-03-21 18:57:32 +0100

    ex00 done

---
+++ CPP_02/ex00/Fixed.cpp
@@ -0,0 +1,38 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	_fixed = 0;
+	std::cout << "constructor called\n";
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "constructor called\n";
+}
+
+Fixed::Fixed(const Fixed &obj)
+{
+	std::cout << "copy constructor called\n";
+	_fixed = obj._fixed;
+}
+
+Fixed& Fixed::operator=(const Fixed &obj)
+{
+	if (this == &obj)
+		return *this;
+	_fixed = getRawBits();
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits called\n";
+	return (_fixed);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits called\n";
+	_fixed = raw;
+}
---
+++ CPP_02/ex00/Fixed.hpp
@@ -0,0 +1,21 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+	private:
+	int	_fixed;
+	static const int _NumOfBits = 8;
+	public:
+	int	getRawBits(void) const;
+	void setRawBits(int const raw);
+	Fixed(void);
+	Fixed(const Fixed &obj);
+	Fixed& operator=(const Fixed &obj);
+	~Fixed(void);
+};
+
+#endif
