a764de3f8821f18ddc3a925a5e2fbbca3f5d642e
mzapora <mz.zapora@gmail.com>
2026-04-10 19:23:42 +0200

    done

---
+++ CPP_03/ex00/ClapTrap.cpp
@@ -0,0 +1,80 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap(std::string name)
+{
+	std::cout << "Constructor called\n";
+	_name = name;
+	_health = 10;
+	_attack_dmg = 0;
+	_energy = 10;
+}
+ClapTrap::ClapTrap()
+{
+	std::cout << "Default constructor called\n";
+}
+
+ClapTrap::ClapTrap(const ClapTrap &obj)
+{
+	std::cout << "Copy constructor called\n";
+	this->_name = obj._name;
+	this->_health = obj._health;
+	this->_attack_dmg = obj._attack_dmg;
+	this->_energy = obj._energy;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap &obj)
+{
+	std::cout << "Copy assignment operator called\n";
+	if (this == &obj)
+		return (*this);
+	this->_name = obj._name;
+	this->_health = obj._health;
+	this->_attack_dmg = obj._attack_dmg;
+	this->_energy = obj._energy;
+	return (*this);
+}
+
+void		ClapTrap::attack(const std::string& target)
+{
+	if (_energy > 0)
+	{
+		std::cout << "Clap Trap " << _name << "attacks " << target << ", causing " << _attack_dmg << "points of damage\n";
+		_energy--;
+		std::cout << "Energy left " << _energy << "\n";
+	}
+	else
+		std::cout << "No enough points to attack " << target << "\n";
+}
+
+void		ClapTrap::takeDamage(unsigned int amount)
+{
+	if (_health == 0)
+	{
+		std::cout << "ClapTrap " << _name << "doesn't have HP\n";
+		return ;
+	}
+	_health -= amount;
+	if (_health < 0)
+		_health = 0;
+	std::cout << "ClapTrap " << _name << "take " << amount << "points of damage\n";
+	std::cout << "Clap trap has " << _health << "HP left\n";
+}
+
+void		ClapTrap::beRepaired(unsigned int amount)
+{
+	if (_energy < 1)
+	{
+		std::cout << "ClapTrap " << _name << "No energy points left\n";
+		return ;
+	}
+	else
+	{
+		_health += amount;
+		_energy--;
+		std::cout << "ClapTrap " << _name << "repaired 1 point of HP from 1 points of energy\n";
+	}
+}
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor called\n";
+}
---
+++ CPP_03/ex00/ClapTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef CLAP_TRAP
+# define CLAP_TRAP
+
+# include <iostream>
+# include <string>
+
+
+class	ClapTrap
+{
+	private:
+	std::string	_name;
+	int			_health;
+	int			_energy;
+	int			_attack_dmg;
+	public:
+	ClapTrap(std::string name);
+	ClapTrap();
+	ClapTrap(const ClapTrap &obj);
+	ClapTrap& 	operator=(const ClapTrap &obj);
+	void		attack(const std::string& target);
+	void		takeDamage(unsigned int amount);
+	void		beRepaired(unsigned int amount);
+	~ClapTrap();
+};
+
+#endif
