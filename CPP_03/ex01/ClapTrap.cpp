e89378f34b0120f1f8e83e0c3577235067e9b8ea
zapii0 <mz.zapora@gmail.com>
2026-04-27 16:52:32 +0200

    upd

--- CPP_03/ex01/ClapTrap.cpp
+++ CPP_03/ex01/ClapTrap.cpp
@@ -2,45 +2,49 @@
 
 ClapTrap::ClapTrap(std::string name)
 {
-	std::cout << "Constructor called\n";
-	_name = name;
-	_health = 10;
-	_attack_dmg = 0;
-	_energy = 10;
+	this->_name = name;
+	this->_health = 10;
+	this->_attack_dmg = 0;
+	this->_energy = 10;
+	std::cout << "ClapTrap " << this->_name << " constructor called\n";
 }
 ClapTrap::ClapTrap()
 {
-	std::cout << "Default constructor called\n";
+	this->_name = "default";
+	this->_health = 10;
+	this->_attack_dmg = 0;
+	this->_energy = 10;
+	std::cout << "ClapTrap " << this->_name << " default constructor called\n";
 }
 
 ClapTrap::ClapTrap(const ClapTrap &obj)
 {
-	std::cout << "Copy constructor called\n";
 	this->_name = obj._name;
 	this->_health = obj._health;
 	this->_attack_dmg = obj._attack_dmg;
 	this->_energy = obj._energy;
+	std::cout << "ClapTrap " << this->_name << " copy constructor called\n";
 }
 
 ClapTrap& ClapTrap::operator=(const ClapTrap &obj)
 {
-	std::cout << "Copy assignment operator called\n";
 	if (this == &obj)
-		return (*this);
+	return (*this);
 	this->_name = obj._name;
 	this->_health = obj._health;
 	this->_attack_dmg = obj._attack_dmg;
 	this->_energy = obj._energy;
+	std::cout << "ClapTrap " << this->_name << " copy assignment operator called\n";
 	return (*this);
 }
 
 void		ClapTrap::attack(const std::string& target)
 {
-	if (_energy > 0)
+	if (this->_energy > 0)
 	{
-		std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attack_dmg << " points of damage\n";
-		_energy--;
-		std::cout << "Energy left " << _energy << "\n";
+		std::cout << "ClapTrap " << this->_name << " attacks " << target << ", causing " << this->_attack_dmg << " points of damage\n";
+		this->_energy--;
+		std::cout << "Energy left " << this->_energy << "\n";
 	}
 	else
 		std::cout << "No enough points to attack " << target << "\n";
@@ -48,34 +52,34 @@ void		ClapTrap::attack(const std::string& target)
 
 void		ClapTrap::takeDamage(unsigned int amount)
 {
-	if (_health == 0)
+	if (this->_health == 0)
 	{
-		std::cout << "ClapTrap " << _name << "doesn't have HP\n";
+		std::cout << "ClapTrap " << this->_name << "doesn't have HP\n";
 		return ;
 	}
-	_health -= amount;
-	if (_health < 0)
-		_health = 0;
-	std::cout << "ClapTrap " << _name << " take " << amount << " points of damage\n";
-	std::cout << "ClapTrap has " << _health << " HP left\n";
+	this->_health -= amount;
+	if (this->_health < 0)
+		this->_health = 0;
+	std::cout << "ClapTrap " << this->_name << " take " << amount << " points of damage\n";
+	std::cout << "ClapTrap has " << this->_health << " HP left\n";
 }
 
 void		ClapTrap::beRepaired(unsigned int amount)
 {
 	if (_energy < 1)
 	{
-		std::cout << "ClapTrap " << _name << " No energy points left\n";
+		std::cout << "ClapTrap " << this->_name << " No energy points left\n";
 		return ;
 	}
 	else
 	{
 		_health += amount;
 		_energy--;
-		std::cout << "ClapTrap " << _name << " " << " repaired " << amount << " point of HP from " << amount << " points of energy\n";
-		std::cout << "ClapTrap " << _name << " " << _health << " HP level\n";
+		std::cout << "ClapTrap " << this->_name << " " << " repaired " << amount << " point of HP from " << amount << " points of energy\n";
+		std::cout << "ClapTrap " << this->_name << " " << this->_health << " HP level\n";
 	}
 }
 ClapTrap::~ClapTrap()
 {
-	std::cout << "Destructor called\n";
+	std::cout << "ClapTrap " << this->_name << " destructor called\n";
 }
--- CPP_03/ex01/ScavTrap.cpp
+++ CPP_03/ex01/ScavTrap.cpp
@@ -5,26 +5,26 @@ ScavTrap::ScavTrap(std::string name) : ClapTrap(name)
 	this->_health = 100;
 	this->_energy = 50;
 	this->_attack_dmg = 20;
-	std::cout << "ScavTrap constructor called\n";
+	std::cout << "ScavTrap " << this->_name << " constructor called\n";
 }
 
 ScavTrap::~ScavTrap()
 {
-	std::cout << "ScavTrap destructor called\n";
+	std::cout << "ScavTrap " << this->_name << " destructor called\n";
 }
 
 void	ScavTrap::guardGate()
 {
-	std::cout << "ScavTrap is now in Gate keeper mode\n";
+	std::cout << "ScavTrap "<< this->_name << " is now in Gate keeper mode\n";
 }
 
 void		ScavTrap::attack(const std::string& target)
 {
 	if (_energy > 0)
 	{
-		std::cout << "ScavTrap " << _name << " attacks " << target << ", causing " << _attack_dmg << " points of damage\n";
+		std::cout << "ScavTrap " << this->_name << " attacks " << target << ", causing " << this->_attack_dmg << " points of damage\n";
 		_energy--;
-		std::cout << "Energy left " << _energy << "\n";
+		std::cout << "Energy left " << this->_energy << "\n";
 	}
 	else
 		std::cout << "No enough points to attack " << target << "\n";
@@ -33,17 +33,17 @@ void		ScavTrap::attack(const std::string& target)
 
 ScavTrap::ScavTrap(const ScavTrap &obj) : ClapTrap(obj)
 {
-	std::cout << "ScavTrap Copy constructor called\n";
 	this->_name = obj._name;
 	this->_health = obj._health;
 	this->_attack_dmg = obj._attack_dmg;
 	this->_energy = obj._energy;
+	std::cout <<  "ScavTrap " << this->_name << " Copy constructor called\n";
 }
 ScavTrap::ScavTrap()
 {
-	std::cout << "Deafult constructor called\n";
 	this->_name = "deafult";
 	this->_health = 100;
 	this->_energy = 50;
 	this->_attack_dmg = 20;
+	std::cout << this->_name << " deafult constructor called\n";
 }
